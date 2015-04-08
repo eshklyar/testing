65bc88a45354e64c245c060508f0f47b4f55d586
eshklyar <eshklyar@gmail.com>
2015-04-07 21:29:38 -0500

    Initial Commit

---
+++ testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testing
+//
+//  Created by Edik Shklyar on 4/7/15.
+//  Copyright (c) 2015 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testing
+//
+//  Created by Edik Shklyar on 4/7/15.
+//  Copyright (c) 2015 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
